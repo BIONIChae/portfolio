1c7666240d30801e64451aaa95eed91612dd147e
Chaeee <89851362+BIONIChae@users.noreply.github.com>
2023-09-14 21:04:58 +0200

    Changes made to Portfolio

--- README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio Project
-
-## This is my portfolio project. Where you get to know me and what I do. 
--- index.js
+++
@@ -1,6 +0,0 @@
-function webdev() {
-  window.location.href = "webdev.html";
-}
-
-let web = document.querySelector(".web-dev");
-web.addEventListener("click", webdev);
