1dde0b6ded532c346be75ff3f08d54238a77765f
Chaeee <89851362+BIONIChae@users.noreply.github.com>
2023-09-16 08:05:10 +0200

    Some improvements made

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function toggleDarkMode(event) {
   event.preventDefault();
   if (event.target.checked) {
+    image.setAttribute(
+      "src",
+      `https://i.pinimg.com/originals/14/2f/92/142f929303f5b7f3b36677c76535457f.gif`
+    );
     document.querySelector("body").classList.add("darkmode");
   } else {
     document.querySelector("body").classList.remove("darkmode");
+    image.setAttribute("src", `https://i.gifer.com/5Xyr.gif`);
   }
+
+
 }
 
+let image = document.querySelector("img");
 let darkmodeSwitch = document.querySelector("#flexSwitchCheckDefault");
 darkmodeSwitch.addEventListener("change", toggleDarkMode);
